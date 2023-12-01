b3f9e48b6b2d48c3bb8d9bee1ac418ac69ca4db5
MatheeusFariass <143330376+MatheeusFariass@users.noreply.github.com>
2023-11-30 22:11:51 -0300

    Adicionando Js

---
+++ biografia-main/script.js
@@ -0,0 +1,61 @@
+   // Função de mostrar detalhes de informações. 
+
+function mostrarDetalhes() {
+    
+    var detalhesElement = document.getElementById("detalhes");
+    
+       
+        if (detalhesElement.style.display === "none") {
+            detalhesElement.style.display = "block";
+        } else {
+            detalhesElement.style.display = "none";
+        }
+    }
+    function mostrarDetalhes1() {
+        
+        var detalhesElement = document.getElementById("detalhes1");
+
+        
+        if (detalhesElement.style.display === "none") {
+            detalhesElement.style.display = "block";
+        } else {
+            detalhesElement.style.display = "none";
+        }
+    }
+        function mostrarDetalhes2() {
+        
+        var detalhesElement = document.getElementById("detalhes2");
+
+       
+        if (detalhesElement.style.display === "none") {
+            detalhesElement.style.display = "block";
+        } else {
+            detalhesElement.style.display = "none";
+        }
+    }
+
+    function mostrarDetalhes3() {
+       
+        var detalhesElement = document.getElementById("detalhes3");
+
+       
+        if (detalhesElement.style.display === "none") {
+            detalhesElement.style.display = "block";
+        } else {
+            detalhesElement.style.display = "none";
+        }
+    }
+
+    function mostrarDetalhes4() {
+        
+        var detalhesElement = document.getElementById("detalhes4");
+
+        
+        if (detalhesElement.style.display === "none") {
+            detalhesElement.style.display = "block";
+        } else {
+            detalhesElement.style.display = "none";
+        }
+    }
+
+
